0317bac7b58ff3cad7e25dbaa727dc21225a0369
kisha <mathieu.parbez@epfl.ch>
2025-04-10 16:23:06 +0200

    gh

--- DSM_calculs.m
+++ DSM_calculs.m
@@ -24,12 +24,18 @@ z_support2CG = z_sup2 - z_CG; % position du support 2 par rapport au CG
 
 z_balourdCG = z_CGcentre + L_a/2;
 
+Jrz = pi/32*(rho_r*(L_r - L_a)*(D_r^4 - D_i^4) + rho_a*L_a*(D_a^4 - D_i^4) + rho_r*L_a*(D_r^4 - D_a^4));
+k_lx = E_l*L_l*h_l/t_l;
+k_lz = E_l*L_l*t_l/h_l;
+
+
 % Moment d'inertie y rotor 
 
     % découpage du rotor en trois cylindre de même densité. 
     % part 1: arbre de longueur L_r - L_a
     % part 2: arbre de longueur L_a
     % part 3: aimant de longueur L_a
+
 
     % Masses des différentes partie:
     M_1 = (pi/4)*rho_r*(D_r^2 - D_i^2)*(L_r - L_a);
@@ -45,16 +51,8 @@ z_balourdCG = z_CGcentre + L_a/2;
 
     % calcul des inertie au centre de masse du rotor:
 
-    J_y1 = J_y1g + M_1*()
-    
 
-Jrz = pi/32*(rho_r*(L_r - L_a)*(D_r^4 - D_i^4) + rho_a*L_a*(D_a^4 - D_i^4) + rho_r*L_a*(D_r^4 - D_a^4));
-k_lx = E_l*L_l*h_l/t_l;
-k_lz = E_l*L_l*t_l/h_l;
 
-k_cArray = (E_c * t_c * h_c) ./ l_cArray
-
-for i 
     J_y1 = J_y1cg + M_1*(L_a/2 + 0.5*L_r - z_CG)^2;
     J_y2 = J_y2cg + M_2*(z_CG - L_r/2)^2;
     J_y3 = J_y3cg + M_3*(z_CG - L_r/2)^2;
