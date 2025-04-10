67a2bef609655d804fcb3c56d4ae114379e2439e
nicoblon <nicolas.blondon@gmail.com>
2025-04-10 16:27:21 +0200

    remis au propre

--- DSM_calculs.m
+++ DSM_calculs.m
@@ -37,24 +37,28 @@ k_lz = E_l*L_l*t_l/h_l;
     % part 3: aimant de longueur L_a
 
 
-    % Masses des différentes partie:
-    M_1 = (pi/4)*rho_r*(D_r^2 - D_i^2)*(L_r - L_a);
-    M_2 = (pi/4)*rho_r*(D_r^2 - D_a^2)*L_a;
-    M_3 = (pi/4)*rho_a*(D_a^2 - D_i^2)*L_a;
 
     % formule pour un cylindre creux autour de leur centre de gravité: 
     % J_y = J_x = (1/12)*M*(3*R_i + 3*R_e + l^2)
 
-    J_y1cg = (1/12)*M_1*(3*D_r/2 + 3*D_i/2 + (L_r - L_a)^2);
-    J_y2cg = (1/12)*M_2*(3*D_r/2 + 3*D_a/2 + L_a^2);
-    J_y3cg = (1/12)*M_3*(3*D_a/2 + 3*D_i/2 + L_a^2);
+    J_y1cg = (1/12)*m_r2*(3*D_r/2 + 3*D_i/2 + (L_r - L_a)^2);
+    J_y2cg = (1/12)*m_r1*(3*D_r/2 + 3*D_a/2 + L_a^2);
+    J_y3cg = (1/12)*m_a*(3*D_a/2 + 3*D_i/2 + L_a^2);
 
     % calcul des inertie au centre de masse du rotor:
 
     
     
 
-Jrz = pi/32*(rho_r*(L_r - L_a)*(D_r^4 - D_i^4) + rho_a*L_a*(D_a^4 - D_i^4) + rho_r*L_a*(D_r^4 - D_a^4));
+
+
+    J_y1 = J_y1cg + m_r2*(L_a/2 + 0.5*L_r - z_CG)^2;
+    J_y2 = J_y2cg + m_r1*(z_CG - L_r/2)^2;
+    J_y3 = J_y3cg + m_a*(z_CG - L_r/2)^2;
+
+    J_ry = J_y1 + J_y2 + J_y3 % Inertie totale du rotor autour de y
+
+    Jrz = pi/32*(rho_r*(L_r - L_a)*(D_r^4 - D_i^4) + rho_a*L_a*(D_a^4 - D_i^4) + rho_r*L_a*(D_r^4 - D_a^4));
 k_lx = E_l*L_l*h_l/t_l;
 k_lz = E_l*L_l*t_l/h_l;
 
@@ -65,10 +69,3 @@ k_c4 = E_c * h_c * t_c / l_c4;
 k_c5 = E_c * h_c * t_c / l_c5;
 k_c6 = E_c * h_c * t_c / l_c6;
 k_c7 = E_c * h_c * t_c / l_c7;
-
-
-    J_y1 = J_y1cg + M_1*(L_a/2 + 0.5*L_r - z_CG)^2;
-    J_y2 = J_y2cg + M_2*(z_CG - L_r/2)^2;
-    J_y3 = J_y3cg + M_3*(z_CG - L_r/2)^2;
-
-    J_ry = J_y1 + J_y2 + J_y3 % Inertie totale du rotor autour de y
