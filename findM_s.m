82c0fd94ea50e98291209fb5def83bccf1859d82
Edoardo Maserati <edoardomaserati04@gmail.com>
2025-05-17 16:04:10 +0200

    edo

--- findM_s.m
+++ findM_s.m
@@ -16,14 +16,19 @@ function [m_s,boolWorks] = findM_s(plageBalourd, etendue_mesure, m_r, L_s1, L_s2
     accel_minVect = omega^4 * plageBalourd(1) * (invertedMatrix * D);
     accel_maxVect = omega^4 * plageBalourd(2) * (invertedMatrix * D);
     
-    accel_max = 
+    accel_min_x = accel_minVect(1)
+    accel_max_x = accel_maxVect(1)
+    accel_min_theta = accel_minVect(2)
+    accel_min_theta = accel_maxVect(2)
 
     % accel_minVect est un vecteur -> on compare quelle accélération avec
     % les étendues de mesure 
 
     % on prend les accel en direction de x en x1 et x2?
+    
+    
 
-    if(accel_max <= etendue_mesure(2) && accel_min >= etendue_mesure(1))
+    if(accel_max_x <= etendue_mesure(2) && accel_min_x >= etendue_mesure(1) && accel_max_theta <= etendue_mesure(2) && accel_min_theta >= etendue_mesure(1) )
         boolWorks = true;
     else
         boolWorks = false;
